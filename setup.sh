475b350a9f116c2b080e828afe70a27f04691e35
Lukáš Doležel <dolezlu@gmail.com>
2022-07-15 14:31:28 +0200

    promptless ssh-keygen

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt install -y qemu-kvm qemu-system \
 libvirt-daemon-system libvirt-clients bridge-utils \
 libguestfs-tools python3-libvirt
 
-# Pridani repozitare a instalace Ansible
+# Pridani repozitare a instalace Ansible a komunitnich modulu
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt update
@@ -14,7 +14,7 @@ apt install -y ansible
 ansible-galaxy collection install community.libvirt
 ansible-galaxy collection install community.mysql
 
-# Vytvoreni SSH klicu
-ssh-keygen
+# Vytvoreni SSH klicu, nyni bez vyzvy
+ssh-keygen -q -t rsa -b 4096 -N '' -f /root/.ssh/id_rsa 
 
 virsh net-start default
